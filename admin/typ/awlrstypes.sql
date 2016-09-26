b699453deb184a2da16048d4afc24c03457da4b4
Mike.Huitson <Mike.Huitson@eaf45781-43f6-f040-84cf-876e5d29dca9>
2016-09-26 17:18:44 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/awlrs/trunk@6 eaf45781-43f6-f040-84cf-876e5d29dca9

---
+++ admin/typ/awlrstypes.sql
@@ -0,0 +1,75 @@
+-------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //new_vm_latest/archives/awlrs/admin/typ/awlrstypes.sql-arc   1.0   26 Sep 2016 18:18:44   Mike.Huitson  $
+--       Module Name      : $Workfile:   awlrstypes.sql  $
+--       Date into PVCS   : $Date:   26 Sep 2016 18:18:44  $
+--       Date fetched Out : $Modtime:   26 Sep 2016 17:14:48  $
+--       Version          : $Revision:   1.0  $
+-------------------------------------------------------------------------
+--   Copyright (c) 2016 Bentley Systems Incorporated. All rights reserved.
+-------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT Dropping Existing Types
+SET TERM OFF
+--
+BEGIN
+  EXECUTE IMMEDIATE('DROP TYPE AWLRS_MESSAGE_TAB');
+EXCEPTION
+  WHEN others
+   THEN
+      NULL;
+END;
+/
+--
+BEGIN
+  EXECUTE IMMEDIATE('DROP TYPE AWLRS_MESSAGE');
+EXCEPTION
+  WHEN others
+   THEN
+      NULL;
+END;
+/
+--
+--------------------------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT awlrs_message_type header
+SET TERM OFF
+SET DEFINE ON
+SET FEEDBACK OFF
+SELECT '&exor_base'||'awlrs'||'&terminator'||'admin'||'&terminator'||'typ'
+       ||'&terminator'||'awlrs_message.tyh' run_file
+  FROM dual
+     ;
+START '&&run_file'
+--
+--------------------------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT awlrs_message_tab header
+SET TERM OFF
+SET DEFINE ON
+SET FEEDBACK OFF
+SELECT '&exor_base'||'awlrs'||'&terminator'||'admin'||'&terminator'||'typ'
+       ||'&terminator'||'awlrs_message_tab.tyh' run_file
+  FROM dual
+     ;
+START '&&run_file'
+--
+--------------------------------------------------------------------------------------------
+--
+-- *************************************************************************************************************
+-- * new types above here                                                                                      *
+--                                                                                                             *
+-- * Important: if you introduce new types then be aware that this script runs on install and at every upgrade *
+-- * so if your type has dependants - these must be dropped in the correct order at the top of this script     *
+-- *************************************************************************************************************
+--
+SET TERM ON
+
+
+
+
+
