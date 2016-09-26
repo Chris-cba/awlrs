dabebfc05ee0f6153309bc36335e87c989b59ea3
Mike.Huitson <Mike.Huitson@eaf45781-43f6-f040-84cf-876e5d29dca9>
2016-09-26 17:20:42 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/awlrs/trunk@8 eaf45781-43f6-f040-84cf-876e5d29dca9

---
+++ install/awlrsdata2.sql
@@ -0,0 +1,53 @@
+-------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //new_vm_latest/archives/awlrs/install/awlrsdata2.sql-arc   1.0   26 Sep 2016 18:20:42   Mike.Huitson  $
+--       Module Name      : $Workfile:   awlrsdata2.sql  $
+--       Date into PVCS   : $Date:   26 Sep 2016 18:20:42  $
+--       Date fetched Out : $Modtime:   26 Sep 2016 12:37:26  $
+--       Version          : $Revision:   1.0  $
+--       Table Owner      : AWLRS_METADATA
+--       Generation Date  : 26-SEP-2016 12:37
+--
+--   Product metadata script
+--   As at Release 4.7.0.0
+--
+-------------------------------------------------------------------------
+--   Copyright (c) 2016 Bentley Systems Incorporated. All rights reserved.
+-------------------------------------------------------------------------
+--
+--   TABLES PROCESSED
+--   ================
+--   NM_ERRORS
+--
+-----------------------------------------------------------------------------
+--
+SET define OFF;
+SET feedback OFF;
+---------------------------------
+-- START OF GENERATED METADATA --
+---------------------------------
+--
+----------------------------------------------------------------------------------------
+-- NM_ERRORS
+--
+-- select * from awlrs_metadata.nm_errors
+-- order by ner_appl
+--         ,ner_id
+--
+----------------------------------------------------------------------------------------
+SET TERM ON
+PROMPT nm_errors
+SET TERM OFF
+--
+----------------------------------------------------------------------------------------
+--
+COMMIT;
+--
+SET feedback ON
+SET define ON
+--
+-------------------------------
+-- END OF GENERATED METADATA --
+-------------------------------
+--
