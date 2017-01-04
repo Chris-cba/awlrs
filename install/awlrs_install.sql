23c2618616fd7353e3bd44931d897ee6635e2da0
Peter.Bibby <Peter.Bibby@eaf45781-43f6-f040-84cf-876e5d29dca9>
2017-01-04 14:31:26 +0000

    v4.7.2.1
    git-svn-id: svn://127.0.0.1/awlrs/trunk@125 eaf45781-43f6-f040-84cf-876e5d29dca9

--- install/awlrs_install.sql
+++ install/awlrs_install.sql
@@ -1,11 +1,11 @@
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //new_vm_latest/archives/awlrs/install/awlrs_install.sql-arc   1.1   Dec 02 2016 12:04:36   Peter.Bibby  $
+--       PVCS id          : $Header:   //new_vm_latest/archives/awlrs/install/awlrs_install.sql-arc   1.2   Jan 04 2017 14:31:26   Peter.Bibby  $
 --       Module Name      : $Workfile:   awlrs_install.sql  $
---       Date into PVCS   : $Date:   Dec 02 2016 12:04:36  $
---       Date fetched Out : $Modtime:   Dec 02 2016 12:04:12  $
---       Version          : $Revision:   1.1  $
+--       Date into PVCS   : $Date:   Jan 04 2017 14:31:26  $
+--       Date fetched Out : $Modtime:   Jan 04 2017 13:57:24  $
+--       Version          : $Revision:   1.2  $
 -------------------------------------------------------------------------
 --   Copyright (c) 2016 Bentley Systems Incorporated. All rights reserved.
 -------------------------------------------------------------------------
@@ -273,7 +273,7 @@ SET TERM ON
 Prompt Setting The Version Number...
 SET TERM OFF
 BEGIN
-  hig2.upgrade('AWLRS','awlrs_install.sql','Installed','4.7.2.0');
+  hig2.upgrade('AWLRS','awlrs_install.sql','Installed','4.7.2.1');
 END;
 /
 COMMIT;
