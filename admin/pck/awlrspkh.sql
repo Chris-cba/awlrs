e89a45261a1667228d5db1f5c228923d82940e57
Mike.Huitson <Mike.Huitson@eaf45781-43f6-f040-84cf-876e5d29dca9>
2017-02-27 11:15:18 +0000

    Added awlrs_search_api
    git-svn-id: svn://127.0.0.1/awlrs/trunk@280 eaf45781-43f6-f040-84cf-876e5d29dca9

--- admin/pck/awlrspkh.sql
+++ admin/pck/awlrspkh.sql
@@ -1,11 +1,11 @@
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //new_vm_latest/archives/awlrs/admin/pck/awlrspkh.sql-arc   1.5   02 Feb 2017 10:03:04   Mike.Huitson  $
+--       PVCS id          : $Header:   //new_vm_latest/archives/awlrs/admin/pck/awlrspkh.sql-arc   1.6   27 Feb 2017 11:15:18   Mike.Huitson  $
 --       Module Name      : $Workfile:   awlrspkh.sql  $
---       Date into PVCS   : $Date:   02 Feb 2017 10:03:04  $
---       Date fetched Out : $Modtime:   02 Feb 2017 09:50:24  $
---       Version          : $Revision:   1.5  $
+--       Date into PVCS   : $Date:   27 Feb 2017 11:15:18  $
+--       Date fetched Out : $Modtime:   27 Feb 2017 11:14:48  $
+--       Version          : $Revision:   1.6  $
 -------------------------------------------------------------------------
 --   Copyright (c) 2017 Bentley Systems Incorporated. All rights reserved.
 -------------------------------------------------------------------------
@@ -248,6 +248,20 @@ SET TERM OFF
 SET define ON
 SELECT '&exor_base'||'awlrs'||'&terminator'||'admin'||'&terminator'||'pck'
        ||'&terminator'||'awlrs_reshape_api.pkh' run_file
+  FROM dual
+     ;
+SET feedback ON
+START '&run_file'
+SET feedback OFF
+--
+-------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT awlrs_search_api
+SET TERM OFF
+SET define ON
+SELECT '&exor_base'||'awlrs'||'&terminator'||'admin'||'&terminator'||'pck'
+       ||'&terminator'||'awlrs_search_api.pkh' run_file
   FROM dual
      ;
 SET feedback ON
