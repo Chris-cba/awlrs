2062fe1680336ad23ced5775fe7c3e5e782baf5b
Mike.Huitson <Mike.Huitson@eaf45781-43f6-f040-84cf-876e5d29dca9>
2017-06-15 18:26:40 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/awlrs/trunk@451 eaf45781-43f6-f040-84cf-876e5d29dca9

---
+++ install/awlrs1004_awlrs1005_ddl_upg.sql
@@ -0,0 +1,27 @@
+------------------------------------------------------------------
+-- awlrs1004_awlrs1005_ddl_upg.sql
+------------------------------------------------------------------
+
+
+------------------------------------------------------------------
+--
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //new_vm_latest/archives/awlrs/install/awlrs1004_awlrs1005_ddl_upg.sql-arc   1.0   15 Jun 2017 19:26:40   Mike.Huitson  $
+--       Module Name      : $Workfile:   awlrs1004_awlrs1005_ddl_upg.sql  $
+--       Date into PVCS   : $Date:   15 Jun 2017 19:26:40  $
+--       Date fetched Out : $Modtime:   15 Jun 2017 19:14:26  $
+--       Version          : $Revision:   1.0  $
+--
+------------------------------------------------------------------
+--  Copyright (c) 2017 Bentley Systems Incorporated. All rights reserved.
+
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+
+------------------------------------------------------------------
+-- end of script 
+------------------------------------------------------------------
+
