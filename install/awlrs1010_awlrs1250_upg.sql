5cd1e8b8a00b910bf8852a58d0cc5d94e5f63341
Mike.Huitson <Mike.Huitson@eaf45781-43f6-f040-84cf-876e5d29dca9>
2019-04-01 13:25:46 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/awlrs/trunk@981 eaf45781-43f6-f040-84cf-876e5d29dca9

---
+++ install/awlrs1010_awlrs1250_upg.sql
@@ -0,0 +1,202 @@
+--
+-----------------------------------------------------------------------------
+--
+--   PVCS Identifiers :-
+--
+--       PVCS id          : $Header:   //new_vm_latest/archives/awlrs/install/awlrs1010_awlrs1250_upg.sql-arc   1.0   Apr 01 2019 14:25:46   Mike.Huitson  $
+--       Module Name      : $Workfile:   awlrs1010_awlrs1250_upg.sql  $
+--       Date into PVCS   : $Date:   Apr 01 2019 14:25:46  $
+--       Date fetched Out : $Modtime:   Apr 01 2019 14:22:54  $
+--       Version          : $Revision:   1.0  $
+--
+--   Product upgrade script
+--
+-----------------------------------------------------------------------------
+--  Copyright (c) 2019 Bentley Systems Incorporated. All rights reserved.
+-----------------------------------------------------------------------------
+SET ECHO OFF
+SET LINESIZE 120
+SET HEADING OFF
+SET FEEDBACK OFF
+-- Grab date/time to append to log file names this is standard to all upgrade/install scripts
+undefine log_extension
+col         log_extension new_value log_extension noprint
+set term off
+select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
+/
+set term on
+---------------------------------------------------------------------------------------------------
+-- Spool to Logfile
+define logfile1='awlrs1010_awlrs1250_1_&log_extension'
+define logfile2='awlrs1010_awlrs1250_2_&log_extension'
+spool &logfile1
+--get some db info
+SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+FROM v$instance;
+SELECT 'Current version of '||hpr_product||' ' ||hpr_version
+FROM hig_products
+WHERE hpr_product = 'AWLRS';
+---------------------------------------------------------------------------------------------------
+--                        ****************   CHECK(S)   *******************
+WHENEVER SQLERROR EXIT
+begin
+   hig2.pre_upgrade_check (p_product               => 'AWLRS'
+                          ,p_new_version           => '1.2.5.0'
+                          ,p_allowed_old_version_1 => '1.0.1.0'
+                          ,p_allowed_old_version_2 => '1.1.6.1'
+                          ,p_allowed_old_version_3 => '1.2.4.1'
+                          );
+END;
+/
+WHENEVER SQLERROR CONTINUE
+--
+---------------------------------------------------------------------------------------------------
+--                        **************** TYPES   ****************
+--
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'awlrs'||'&terminator'||'admin'||
+        '&terminator'||'typ'||'&terminator'||'awlrstypes.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   DDL   *******************
+SET TERM ON
+PROMPT DDL Changes...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'awlrs'||'&terminator'||'install'||
+        '&terminator'||'awlrs1010_awlrs1250_ddl_upg.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                  **************** PACKAGE HEADERS AND BODIES   ****************
+--
+SET TERM ON
+PROMPT Package Headers...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'awlrs'||'&terminator'||'admin'||
+       '&terminator'||'pck'||'&terminator'||'awlrspkh.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT Package Bodies...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'awlrs'||'&terminator'||'admin'||
+       '&terminator'||'pck'||'&terminator'||'awlrspkb.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   COMPILE SCHEMA   *******************
+SET TERM ON
+Prompt Creating Compiling Schema Script...
+SET TERM OFF
+SPOOL OFF
+SET define ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'admin'||
+        '&terminator'||'utl'||'&terminator'||'compile_schema.sql' run_file
+FROM dual
+/
+start '&run_file'
+spool &logfile2
+SET TERM ON
+start compile_all.sql
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   CONTEXT   *******************
+--The compile_all will have reset the user context so we must reinitialise it
+--
+SET FEEDBACK OFF
+SET TERM ON
+PROMPT Reinitialising Context...
+SET TERM OFF
+BEGIN
+  nm3context.initialise_context;
+  nm3user.instantiate_user;
+END;
+/
+---------------------------------------------------------------------------------------------------
+--                  ****************   METADATA  *******************
+SET TERM ON
+PROMPT Metadata Changes...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'awlrs'||'&terminator'||'install'||
+        '&terminator'||'awlrs1010_awlrs1250_metadata_upg.sql' run_file
+FROM dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   SYNONYMS   *******************
+SET TERM ON
+Prompt Creating Synonyms That Do Not Exist...
+SET TERM OFF
+EXECUTE nm3ddl.refresh_all_synonyms;
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   ROLES   *******************
+SET TERM ON
+Prompt Updating HIG_USER_ROLES...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+    '&terminator'||'hig_user_roles.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+Prompt Ensuring all users have HIG_USER role...
+SET TERM OFF
+SET DEFINE ON
+SELECT '&exor_base'||'nm3'||'&terminator'||'install'||
+    '&terminator'||'users_without_hig_user.sql' run_file
+from dual
+/
+SET FEEDBACK ON
+start &&run_file
+SET FEEDBACK OFF
+--
+---------------------------------------------------------------------------------------------------
+--                        ****************   VERSION NUMBER   *******************
+SET TERM ON
+Prompt Setting The Version Number...
+SET TERM OFF
+BEGIN
+      hig2.upgrade('AWLRS','awlrs1010_awlrs1250_upg.sql','Upgrade from 1.0.1.0 to 1.2.5.1','1.2.5.1');
+END;
+/
+COMMIT;
+SET HEADING OFF
+SELECT 'Product updated to version '||hpr_product||' ' ||hpr_version product_version
+FROM hig_products
+WHERE hpr_product = 'AWLRS';
+spool off
+exit
+---------------------------------------------------------------------------------------------------
+--                        ****************   END OF SCRIPT   *******************
