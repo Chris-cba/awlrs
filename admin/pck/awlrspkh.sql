3605a8ce352b0c316071336b4c34b2779faed8a0
Barbara.Odriscoll <Barbara.Odriscoll@eaf45781-43f6-f040-84cf-876e5d29dca9>
2020-05-04 08:34:22 +0000

    Added awlrs_theme_api, awlrs_process_framework_api packages
    git-svn-id: svn://127.0.0.1/awlrs/trunk@1450 eaf45781-43f6-f040-84cf-876e5d29dca9

--- admin/pck/awlrspkh.sql
+++ admin/pck/awlrspkh.sql
@@ -1,11 +1,11 @@
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //new_vm_latest/archives/awlrs/admin/pck/awlrspkh.sql-arc   1.15   Mar 18 2020 07:56:26   Vikas.Mhetre  $
+--       PVCS id          : $Header:   //new_vm_latest/archives/awlrs/admin/pck/awlrspkh.sql-arc   1.16   May 04 2020 09:34:22   Barbara.Odriscoll  $
 --       Module Name      : $Workfile:   awlrspkh.sql  $
---       Date into PVCS   : $Date:   Mar 18 2020 07:56:26  $
---       Date fetched Out : $Modtime:   Mar 18 2020 07:54:08  $
---       Version          : $Revision:   1.15  $
+--       Date into PVCS   : $Date:   May 04 2020 09:34:22  $
+--       Date fetched Out : $Modtime:   May 04 2020 09:30:34  $
+--       Version          : $Revision:   1.16  $
 -------------------------------------------------------------------------
 --   Copyright (c) 2017 Bentley Systems Incorporated. All rights reserved.
 -------------------------------------------------------------------------
@@ -458,6 +458,34 @@ SET TERM OFF
 SET define ON
 SELECT '&exor_base'||'awlrs'||'&terminator'||'admin'||'&terminator'||'pck'
        ||'&terminator'||'awlrs_sdl_upload_api.pkh' run_file
+  FROM dual
+     ;
+SET feedback ON
+START '&run_file'
+SET feedback OFF
+--
+-------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT awlrs_process_framework_api
+SET TERM OFF
+SET define ON
+SELECT '&exor_base'||'awlrs'||'&terminator'||'admin'||'&terminator'||'pck'
+       ||'&terminator'||'awlrs_process_framework_api.pkh' run_file
+  FROM dual
+     ;
+SET feedback ON
+START '&run_file'
+SET feedback OFF
+--
+-------------------------------------------------------------------------
+--
+SET TERM ON
+PROMPT awlrs_theme_api
+SET TERM OFF
+SET define ON
+SELECT '&exor_base'||'awlrs'||'&terminator'||'admin'||'&terminator'||'pck'
+       ||'&terminator'||'awlrs_theme_api.pkh' run_file
   FROM dual
      ;
 SET feedback ON
